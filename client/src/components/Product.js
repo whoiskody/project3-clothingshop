d1c12519711a0b378732f7a8b9a66c0aff9ed5d3
kordaro mitchell <kody_mitchell@ymail.com>
2019-07-25 15:07:22 -0400

    product DELETE added

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -16,11 +16,26 @@ export default class Product extends Component {
             })
     }
 
+    handleDeleteProduct = () => {
+        axios.delete(`/api/shops/${this.props.match.params.shopId}/products/${this.props.match.params.productId}`)
+        .then(() => {
+            this.setState({redirectToHome: true})
+        })
+    }
+
     render() {
+
+        if(this.state.redirectToHome) {
+            return <Redirect to='/' />
+        }
+
         return (
+            
             <div>
-                <h1>YEAaaaaaaa</h1>
-                <h2>{this.state.product.name}</h2>
+                <button onClick={this.handleDeleteProduct}>Delete Product</button>
+                <img src={this.state.product.image}/>
+                <h1>{this.state.product.name}</h1>
+                <p>{this.state.product.description}</p>
             </div>
         )
     }
